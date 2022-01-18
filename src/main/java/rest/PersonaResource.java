f928f1a640518dfd059c489ff5c0588c3ef016f3
leoAlvTor <calvaradot1@est.ups.edu.ec>
2022-01-17 23:32:19 -0600

    commit

--- src/main/java/rest/PersonaResource.java
+++ src/main/java/rest/PersonaResource.java
@@ -9,6 +9,7 @@ import javax.json.bind.JsonbBuilder;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Optional;
 
 @Path("/persona")
 public class PersonaResource {
@@ -34,7 +35,6 @@ public class PersonaResource {
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public Response createPersona(@FormParam("cedula") String cedula, @FormParam("nombre") String nombre,
                                   @FormParam("direccion") String direccion, @FormParam("telefono") String telefono){
-        Jsonb jsonb = JsonbBuilder.create();
         Persona persona = new Persona();
 
         persona.setCedula(cedula);
@@ -45,9 +45,62 @@ public class PersonaResource {
         boolean isCreated = personaFacade.create(persona);
 
         if(isCreated){
-            return Response.ok("PERSON CREATED").build();
+            return Response.ok("PERSON CREATED")
+                    .header("Access-Control-Allow-Origins", "*")
+                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+                    .build();
+        }else{
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("ERROR").build();
         }
 
     }
 
+    @PUT
+    @Path("/update")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response updatePersona(@FormParam("cedula") String cedula, @FormParam("nombre") String nombre,
+                                  @FormParam("direccion") String direccion, @FormParam("telefono") String telefono){
+        Optional<Persona> personaOptional = personaFacade.find(cedula);
+        if(personaOptional.isPresent()){
+            Persona persona = personaOptional.get();
+            persona.setNombre(nombre);
+            persona.setDireccion(direccion);
+            persona.setTelefono(telefono);
+            boolean isUpdated = personaFacade.edit(persona);
+            if(isUpdated){
+                return Response.ok("PERSON UPDATED").header("Access-Control-Allow-Origins", "*")
+                        .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                        .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+                        .build();
+            }else{
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("ERROR").build();
+            }
+        }else{
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("ERROR").build();
+        }
+    }
+
+    @DELETE
+    @Path("/delete")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response deletePersona(@FormParam("cedula") String cedula){
+        Optional<Persona> personaOptional = personaFacade.find(cedula);
+        if(personaOptional.isPresent()){
+            boolean isDeleted = personaFacade.remove(personaOptional.get());
+            if(isDeleted){
+                return Response.ok("PERSON DELETED")
+                        .header("Access-Control-Allow-Origins", "*")
+                        .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                        .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+                        .build();
+            }else{
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("ERROR").build();
+            }
+        }else{
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("ERROR").build();
+        }
+    }
 }
